946dedc456f1f9980a352382e16ba0fb6e603ea9
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-30 16:27:02 -0700

    config main file with navigation

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { StackNavigator } from 'react-navigation'
+
+import FeedsList from './screens/FeedsList'
+import FeedDetail from './screens/FeedDetail'
+import EntryDetail from '/screens/EntryDetail'
+import AddFeed from './screens/AddFeed'
+
+import store from './store'
+
+const Navigator = StackNavigator({
+  FeedsList: { screen: FeedsList },
+  FeedDetail: { screen: FeedDetail },
+  EntryDetail: { screen: EntryDetail },
+  AddFeed: { screen: AddFeed }
+})
+
+export default class App extends Component {
+  render() {
+    return <Navigator screenProps={{ store }} />
+  }
+}
