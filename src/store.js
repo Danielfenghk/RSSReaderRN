e62c1c5543325f5a244c79fba752872928c88553
cdrainxv <cdrainxv@users.noreply.github.com>
2017-12-30 16:02:27 -0700

    add, select, remove, persist methods to update store

--- src/store.js
+++ src/store.js
@@ -1,19 +1,36 @@
 import { observable } from 'mobx'
+import { AsyncStorage } from ' react-native'
 
 class Store {
   @observable feeds
+  @observable selectedFeeds
+  @observable selectedEntry
 
   constructor() {
-    this.feeds = []
+    AsyncStorage.getItem('@feeds').then(sFeeds => {
+      this.feeds = JSON.parse(sFeeds) || []
+    })
+  }
+
+  _persistFeeds() {
+    AsyncStorage.setItem('@feeds', JSON.stringify(this.feeds))
   }
 
   addFeed(url, feed) {
-    const { entry, title, update } = feed
-    this.feeds.push({
-      url,
-      entry,
-      updated
-    })
+    const { entry, title, updated } = feed
+    this.feeds.push({ url, entry, updated })
     this._persistFeeds()
   }
+
+  removeFeed(url) {
+    this.feeds = this.feeds.filter(feed => feed.url !== url)
+    this._persistFeeds()
+  }
+
+  selectEntry(entry) {
+    this.selectEntry = entry
+  }
 }
+
+const store = new Store()
+export default store
